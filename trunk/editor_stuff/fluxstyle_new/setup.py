37c22c8b91774f8b145e962c9bd8b3db581ee91c
errr <errr@293fa9de-b9f7-0310-b153-e5b3fddb211d>
2006-06-23 07:56:10 +0000

    Adding editor stuff code this stuff needs serious work, it will be unstable for a while the stuff added is mostly just me testing how certian widgets work

---
+++ trunk/editor_stuff/fluxstyle_new/setup.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+"""Fluxstyle is a graphical style manager built in python using pygtk and glade.
+Fluxstyle is for the fluxbox window manager. Orignal version written by Michael Rice.
+Many special thanks to Zan a.k.a. Lauri Peltonen for GUI Improvements & Bug Stomping.
+
+Released under GPL"""
+from distutils.core import setup
+
+doclines = __doc__.split("\n")
+
+setup(name='fluxstyle',
+      version='1.0',
+      description=doclines[0],
+      author='Michael Rice',
+      author_email='errr@errr-online.com',
+      url='http://fluxstyle.berlios.de/',
+      packages=['fluxstyle'],
+      data_files=[('/usr/share/fluxstyle-1.0/images',
+        ['images/fluxmetal.png','images/mini-fluxbox6.png','images/none.jpg']),
+        ('/usr/share/fluxstyle-1.0/glade',['images/main.glade']),
+        ('/usr/bin',['fluxStyle']),
+        ('/usr/share/fluxstyle-1.0/docs',['docs/README','docs/LICENSE','docs/Changelog'])]
+      )
